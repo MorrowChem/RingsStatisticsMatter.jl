0605bd705403dc941d2b8ac73fd9017bedf8a892
MorrowChem <joemorrow117@gmail.com>
2023-08-14 08:37:25 +0100

    save rings and add timing

--- run_rings.py
+++ run_rings.py
@@ -1,6 +1,8 @@
 import argparse
 from julia_rings.rings import ring_statistics
 from ase.io import read
+import time
+import numpy as np
 
 parser = argparse.ArgumentParser(description="Ring Statistics Script")
 parser.add_argument("input_file", help="Input file in ASE-supported format")
@@ -8,4 +10,10 @@ args = parser.parse_args()
 
 ats = read(args.input_file, "-1")
 
+st = time.time()
 rs, rings = ring_statistics(ats)
+np.savetxt('rings_stats.npy', rs)
+et = time.time()
+
+elapsed = et - st
+print(f"Elapsed time: {et:.2f} seconds")
